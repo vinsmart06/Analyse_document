eb873a9eca7c8b6093b18dbec566ac1620c519fd
vinsmart06 <vinsmart06@gmail.com>
2026-02-24 00:28:13 +0530

    Update Document_analyser.py

--- Document_analyser.py
+++ Document_analyser.py
@@ -32,7 +32,7 @@ st.set_page_config(
 )
 
 st.title("📊 Enterprise Document Analytics")
-st.caption("Powered by AI LLM + LangChain")
+st.caption("Powered by AI LLM + LangChain: Developed By Vineet Garg")
 
 # ----------------------------
 # SESSION STATE
